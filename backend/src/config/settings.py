8229cff75c76719dd105e9f41c99636f3ec0b2f3
T0337485 <t0337485@my.ntu.ac.uk>
2025-12-25 00:12:24 +0300

    added settings

---
+++ backend/src/config/settings.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_KEY = os.getenv("PINECONE_KEY")
+HUGGINGFACE_KEY = os.getenv("HUGGINGFACE_KEY")
+LLM_KEY = os.getenv("LLM_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+INDEX_NAME = "smart_support"
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 100
