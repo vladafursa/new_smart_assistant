a1288ed886f659e220578c12925f91ce505ce62d
T0337485 <t0337485@my.ntu.ac.uk>
2025-12-25 02:57:06 +0300

    refactoring: added comments and order

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -2,14 +2,19 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-BUCKET_NAME = "files"
-SUPABASE_TABLE = "documents"
-PINECONE_KEY = os.getenv("PINECONE_KEY")
-HUGGINGFACE_KEY = os.getenv("HUGGINGFACE_KEY")
-LLM_KEY = os.getenv("LLM_KEY")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
+# constants
+# str
+BUCKET_NAME = "files"
 INDEX_NAME = "smart_support"
+SUPABASE_TABLE = "documents"
+# int
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 100
+
+# keys
+HUGGINGFACE_KEY = os.getenv("HUGGINGFACE_KEY")
+LLM_KEY = os.getenv("LLM_KEY")
+PINECONE_KEY = os.getenv("PINECONE_KEY")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
