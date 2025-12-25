4f61cf2f80d8ce62f47e8a93d10ab09a97b97958
T0337485 <t0337485@my.ntu.ac.uk>
2025-12-26 01:32:50 +0300

    added dropbox

--- frontend/src/pages/Storage.jsx
+++ frontend/src/pages/Storage.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header.jsx'
+import DropBox from '../components/DropBox.jsx'
 
 const Storage = () => {
 return ( 
     <div>
    <Header></Header>
+    <DropBox></DropBox>
    </div>
 )
 }
