77ae4555c5969246d09ed66542b640c1546ac038
T0337485 <t0337485@my.ntu.ac.uk>
2025-12-30 02:19:25 +0300

    added init py files

---
+++ backend/src/ml/rag/__init__.py
@@ -0,0 +1,2 @@
+from .generator import generate_answer
+from .retriever import query_index
--- backend/src/storage/__init__.py
+++ backend/src/storage/__init__.py
@@ -1,2 +1,3 @@
+from .pinecone_storage import init_index
 from .supabase_storage import get_preview_url, list_all_files
 from .unified_storage import unified_upload
